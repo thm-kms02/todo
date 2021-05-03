b373f50a69c16f8b7b490bc850daac72d1a8966a
Colin Kristen <colin.kristen@mni.thm.de>
2021-05-03 15:40:34 +0200

    tests for adding Tasks

--- test_units/test.ts
+++ test_units/test.ts
@@ -24,3 +24,43 @@ describe('Post /create',()=>{
             });
     });
 });
+
+describe('Post /addTask',()=>{
+    it('should add a new Task to Database',  (done)=> {
+        const Task = {
+            user:"1",
+            ueberschrift:"Milch",
+            beschreibung: "Milch einkaufen",
+            prio: "1",
+            category: "1"
+
+        };
+
+        chain.request('http://localhost:8080')
+            .post('/addTask')
+            .send(Task)
+            .end((err,response)=>{
+                if(response.status == 200) {
+                    done()
+                } else {
+                    return err;
+                }
+            });
+    });
+    it('should fail',  (done)=> {
+
+        const Task2 = {
+        }
+
+        chain.request('http://localhost:8080')
+            .post('/addTask')
+            .send(Task2)
+            .end((err,response)=>{
+                if(response.status == 500) {
+                    done()
+                } else {
+                    return err;
+                }
+            });
+    });
+});
