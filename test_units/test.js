560be5119f9699395062025152e69cee352d74c5
Onur <onur.dede@mni.thm.de>
2021-05-09 16:34:05 +0200

    travis build

---
+++ test_units/test.js
@@ -0,0 +1,156 @@
+// tslint:disable-next-line:no-var-requires
+var chain = require('chai');
+// tslint:disable-next-line:no-var-requires
+var chaiHttps = require('chai-http');
+// tslint:disable-next-line:no-var-requires
+var host = require('../server/server');
+chain.should();
+chain.use(chaiHttps);
+describe('Post /create', function () {
+    it('should create a new Account', function (done) {
+        var account = {
+            email: "dieter@web.de",
+            vorname: "Dieter",
+            nachname: "Mainder",
+            passwort: "passwefrg"
+        };
+        chain.request('http://localhost:8080')
+            .post('/create')
+            .send(account)
+            .end(function (err, response) {
+            console.log(response);
+            response.should.have.status(201);
+            done();
+        });
+    });
+    it('Register without email should fail', function (done) {
+        var account = {
+            vorname: "Dieter",
+            nachname: "Mainder",
+            passwort: "passwefrg"
+        };
+        chain.request('http://localhost:8080')
+            .post('/create')
+            .send(account)
+            .end(function (err, response) {
+            response.should.have.status(500);
+            done();
+        });
+    });
+    it('should load tasks of user with id 1', function (done) {
+        var userid = {
+            id: "1"
+        };
+        chain.request('http://localhost:8080')
+            .get('/loadtasks')
+            .send(userid)
+            .end(function (err, response) {
+            response.should.have.status(200);
+            done();
+        });
+    });
+    it('should login a user', function (done) {
+        var login = {
+            email: "dieter@web.de",
+            passwort: "passwefrg"
+        };
+        chain.request('http://localhost:8080')
+            .post('/login')
+            .send(login)
+            .end(function (err, response) {
+            response.should.have.status(500);
+            done();
+        });
+    });
+    it('Login without password should fail', function (done) {
+        var login = {
+            email: "dieter@web.de"
+        };
+        chain.request('http://localhost:8080')
+            .post('/login')
+            .send(login)
+            .end(function (err, response) {
+            response.should.have.status(500);
+            done();
+        });
+    });
+    it('should create a new Category', function (done) {
+        var Category = {
+            newCat: "test"
+        };
+        chain.request('http://localhost:8080')
+            .post('/addCategory')
+            .send(Category)
+            .end(function (err, response) {
+            response.should.have.status(200);
+            done();
+        });
+    });
+    it('should create a new Category', function (done) {
+        var Category = {
+            newCat: ""
+        };
+        chain.request('http://localhost:8080')
+            .post('/addCategory')
+            .send(Category)
+            .end(function (err, response) {
+            response.should.have.status(400);
+            done();
+        });
+    });
+    it('should load and create the task on mainpage if logged in', function (done) {
+        var Todoliste = {
+            id: "",
+            user: "",
+            ueberschrift: "",
+            beschreibung: "",
+            kategorie: "",
+            prio: "",
+        };
+        chain.request('http://localhost:8080')
+            .get('/todoliste')
+            .send(Todoliste)
+            .end(function (err, response) {
+            response.should.have.status(200);
+            done();
+        });
+    });
+    it('should create a new Task', function (done) {
+        var Task = {
+            ueberschrift: "test",
+            beschreibung: "t",
+            category: 1
+        };
+        chain.request('http://localhost:8080')
+            .post('/addTask')
+            .send(Task)
+            .end(function (err, response) {
+            response.should.have.status(200);
+            done();
+        });
+    });
+    it('should not create a new Task', function (done) {
+        var Task = {
+            ueberschrift: "",
+            beschreibung: "",
+            category: 1,
+        };
+        chain.request('http://localhost:8080')
+            .post('/addTask')
+            .send(Task)
+            .end(function (err, response) {
+            response.should.have.status(400);
+            done();
+        });
+    });
+});
+describe('Get/loadTasks)', function () {
+});
+describe('Post /login', function () {
+});
+describe('Post /addCategory', function () {
+});
+describe('Get/todoliste)', function () {
+});
+describe('Post /addTask', function () {
+});
